9327546809930372f112789beb1e3c14f809af2b
Johann Bronsch <johann.bronsch@haw-hamburg.de>
2015-12-17 16:41:19 +0100

    Fixed

--- open-chord/src/de/haw/ttv/main/Chord.java
+++ open-chord/src/de/haw/ttv/main/Chord.java
@@ -10,11 +10,11 @@ import de.uniba.wiai.lspi.chord.service.impl.ChordImpl;
 public class Chord {
 
     private static final String PROTOCOL = URL.KNOWN_PROTOCOLS.get(URL.SOCKET_PROTOCOL);
-    private static final String HOST_IP = "192.168.99.225"; //99
+    private static final String HOST_IP = "192.168.99.99"; //99
     private static final String HOST_PORT = "8080";
     private static final String JOIN_IP = "192.168.99.225";
     private static final String JOIN_PORT = "8181";
-    private static final String MODUS = "create"; // "join" and "create" are valid
+    private static final String MODUS = "join"; // "join" and "create" are valid
     
     private ChordImpl chordImpl;
     private Main main;
